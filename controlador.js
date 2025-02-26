23ccb48d40df9778013d4339921c0b04dd74629a
camicastrillon2314@gmail.com <camicastrillon2314@gmail.com>
2025-02-26 14:56:13 -0500

    cambio de formulario

---
+++ controlador.js
@@ -0,0 +1,5 @@
+//alert("estoy vinculando")
+
+let inputPesoUsuario=document.getElementsById("pesousuario")
+let inoutAlturaUsuario=document.getElementsById("alturausuario")
+let botonFormulario=document.getElementById("boton")
